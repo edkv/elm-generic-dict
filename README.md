642fe8b74f480ebebef5d25f9b660cfdb986b7ba
Eduard Kovalov <ed@edkovalov.com>
2019-08-17 19:18:56 +0300

    Add docs

---
+++ README.md
@@ -0,0 +1,101 @@
+# GenericDict
+
+A wrapper over `Dict` from `elm/core` that allows you to use any types as keys.
+
+Unlike existing solutions, doesn't store functions in the data structure and
+doesn't have any performance implications (except slightly higher memory usage
+because it stores both original keys and their stringified versions), but requires
+you to explicitly pass a `key -> String` function that tells how to convert your
+key type to a `String` to all functions that need it.
+
+
+## Example
+
+```elm
+module User exposing (Id, User, new, idFromString, idToString)
+
+
+-- In reality I would use custom types for `name`, `email`, and `age`.
+-- And I would also add checks for `Id` format!
+
+
+type Id
+    = Id String
+
+
+type User
+    = User
+        { name : String
+        , email : String
+        , age : Int
+        }
+
+
+new : 
+    { name : String
+    , email : String
+    , age : Int
+    }
+    -> User
+new =
+    User
+
+
+idFromString : String -> Id
+idFromString =
+    Id
+
+
+idToString : Id -> String
+idToString (Id string) =
+    string
+```
+
+```elm
+module Data exposing (users)
+
+import User exposing (User)
+import GenericDict as Dict exposing (Dict)
+
+
+users : Dict User.Id User
+users =
+    Dict.fromList User.idToString
+        [ ( User.idFromString "ef5f03aa-30d8-41fa-9730-9074daffbfd2" 
+          , User.new
+              { name = "Bob"
+              , email = "bob@example.com"
+              , age = 25
+              }
+          )
+        , ( User.idFromString "b6c87ad8-d8b8-4f91-a680-6fec7c1aefb6" 
+          , User.new
+              { name = "Alice"
+              , email = "alice@example.com"
+              , age = 22
+              }
+          )
+        , ( User.idFromString "3a84502a-2423-4168-9f5e-1b5fe8eff034" 
+          , User.new
+              { name = "Chuck"
+              , email = "chuck@example.com"
+              , age = 30
+              }
+          )
+        ]
+```
+
+```elm
+-- Get a user
+Dict.get User.idToString id Data.users
+
+-- Add a new user
+Dict.insert
+    User.idToString
+    id
+    (User.new { name = "Eve", email = "eve@example.com", age = 26 })
+    Data.users
+
+-- Remove a user
+Dict.remove User.idToString id Data.users
+```
--- src/GenericDict.elm
+++ src/GenericDict.elm
@@ -1,141 +1,214 @@
 module GenericDict exposing
     ( Dict
-    , diff
-    , empty
-    , filter
-    , foldl
-    , foldr
-    , fromList
-    , get
-    , insert
-    , intersect
-    , isEmpty
-    , keys
-    , map
-    , member
-    , merge
-    , partition
-    , remove
-    , singleton
-    , size
-    , toList
-    , union
-    , update
-    , values
+    , empty, singleton, insert, update, remove
+    , isEmpty, member, get, size
+    , keys, values, toList, fromList
+    , map, foldl, foldr, filter, partition
+    , union, intersect, diff, merge
     )
+
+{-|
+
+@docs Dict
+
+
+# Build
+
+@docs empty, singleton, insert, update, remove
+
+
+# Query
+
+@docs isEmpty, member, get, size
+
+
+# Lists
+
+@docs keys, values, toList, fromList
+
+
+# Transform
+
+@docs map, foldl, foldr, filter, partition
+
+
+# Combine
+
+@docs union, intersect, diff, merge
+
+-}
 
 import Dict
 
 
+{-| A dictionary of keys and values. Keys can be any type.
+-}
 type Dict k v
     = Dict (Dict.Dict String ( k, v ))
 
 
+{-| Create an empty dictionary.
+-}
 empty : Dict k v
 empty =
     Dict Dict.empty
 
 
+{-| Create a dictionary with one key-value pair.
+-}
 singleton : (k -> String) -> k -> v -> Dict k v
 singleton toString key value =
     Dict (Dict.singleton (toString key) ( key, value ))
 
 
+{-| Insert a key-value pair into a dictionary. Replaces value when there is a collision.
+-}
 insert : (k -> String) -> k -> v -> Dict k v -> Dict k v
 insert toString key value (Dict dict) =
     Dict (Dict.insert (toString key) ( key, value ) dict)
 
 
+{-| Update the value of a dictionary for a specific key with a given function.
+-}
 update : (k -> String) -> k -> (Maybe v -> Maybe v) -> Dict k v -> Dict k v
 update toString key fn (Dict dict) =
     Dict (Dict.update (toString key) (Maybe.map Tuple.second >> fn >> Maybe.map (Tuple.pair key)) dict)
 
 
+{-| Remove a key-value pair from a dictionary. If the key is not found, no changes are made.
+-}
 remove : (k -> String) -> k -> Dict k v -> Dict k v
 remove toString key (Dict dict) =
     Dict (Dict.remove (toString key) dict)
 
 
+{-| Determine if a dictionary is empty.
+-}
 isEmpty : Dict k v -> Bool
 isEmpty (Dict dict) =
     Dict.isEmpty dict
 
 
+{-| Determine if a key is in a dictionary.
+-}
 member : (k -> String) -> k -> Dict k v -> Bool
 member toString key (Dict dict) =
     Dict.member (toString key) dict
 
 
+{-| Get the value associated with a key. Returns `Nothing` if the key is not found.
+-}
 get : (k -> String) -> k -> Dict k v -> Maybe v
 get toString key (Dict dict) =
     Dict.get (toString key) dict |> Maybe.map Tuple.second
 
 
+{-| Determine the number of key-value pairs in the dictionary.
+-}
 size : Dict k v -> Int
 size (Dict dict) =
     Dict.size dict
 
 
+{-| Get all of the keys in a dictionary, sorted by their string representation.
+-}
 keys : Dict k v -> List k
 keys =
     foldr (\key value -> (::) key) []
 
 
+{-| Get all of the values in a dictionary, in the order of their stringified keys.
+-}
 values : Dict k v -> List v
 values =
     foldr (\key value -> (::) value) []
 
 
+{-| Convert a dictionary into an association list of key-value pairs, sorted by stringified keys.
+-}
 toList : Dict k v -> List ( k, v )
 toList (Dict dict) =
     Dict.values dict
 
 
+{-| Convert an association list into a dictionary.
+-}
 fromList : (k -> String) -> List ( k, v ) -> Dict k v
 fromList toString =
     List.foldl (\( key, value ) -> insert toString key value) empty
 
 
+{-| Apply a function to all values in a dictionary.
+-}
 map : (k -> a -> b) -> Dict k a -> Dict k b
 map fn (Dict dict) =
     Dict (Dict.map (\_ ( key, value ) -> ( key, fn key value )) dict)
 
 
+{-| Fold over the key-value pairs in a dictionary from the first one to the last one,
+sorted by stringified keys.
+-}
 foldl : (k -> v -> b -> b) -> b -> Dict k v -> b
 foldl fn acc (Dict dict) =
     Dict.foldl (\_ ( key, value ) -> fn key value) acc dict
 
 
+{-| Fold over the key-value pairs in a dictionary from the last one to the first one,
+sorted by stringified keys.
+-}
 foldr : (k -> v -> b -> b) -> b -> Dict k v -> b
 foldr fn acc (Dict dict) =
     Dict.foldr (\_ ( key, value ) -> fn key value) acc dict
 
 
+{-| Keep only the key-value pairs that pass the given test.
+-}
 filter : (k -> v -> Bool) -> Dict k v -> Dict k v
 filter fn (Dict dict) =
     Dict (Dict.filter (\_ ( key, value ) -> fn key value) dict)
 
 
+{-| Partition a dictionary according to some test. The first dictionary contains all
+key-value pairs which passed the test, and the second contains the pairs that did not.
+-}
 partition : (k -> v -> Bool) -> Dict k v -> ( Dict k v, Dict k v )
 partition fn (Dict dict) =
     Dict.partition (\_ ( key, value ) -> fn key value) dict |> Tuple.mapBoth Dict Dict
 
 
+{-| Combine two dictionaries.
+If there is a collision, preference is given to the first dictionary.
+-}
 union : Dict k v -> Dict k v -> Dict k v
 union (Dict dict1) (Dict dict2) =
     Dict (Dict.union dict1 dict2)
 
 
+{-| Keep a key-value pair when its key appears in the second dictionary.
+Preference is given to values in the first dictionary.
+-}
 intersect : Dict k v -> Dict k v -> Dict k v
 intersect (Dict dict1) (Dict dict2) =
     Dict (Dict.intersect dict1 dict2)
 
 
+{-| Keep a key-value pair when its key does not appear in the second dictionary.
+-}
 diff : Dict k v -> Dict k v -> Dict k v
 diff (Dict dict1) (Dict dict2) =
     Dict (Dict.diff dict1 dict2)
 
 
+{-| The most general way of combining two dictionaries. You provide three
+accumulators for when a given key appears:
+
+1.  Only in the left dictionary.
+2.  In both dictionaries.
+3.  Only in the right dictionary.
+
+You then traverse all the keys from lowest to highest, building up whatever you want.
+
+-}
 merge :
     (k -> a -> result -> result)
     -> (k -> a -> b -> result -> result)
